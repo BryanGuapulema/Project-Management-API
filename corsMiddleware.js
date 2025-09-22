e6ad1ac0a60b75ff34550bc4c050601168122106
BryanGuapulema <bryan19012003@gmail.com>
2025-09-22 13:54:26 -0500

    cors middleware

---
+++ corsMiddleware.js
@@ -0,0 +1,17 @@
+import cors from 'cors'
+const ACCEPTED_ORIGINS = [
+    'http://localhost:80',
+    'http://localhost:3000',
+    'http://localhost:8080'
+]
+
+export const corsMiddleware = ({accepted_origins = ACCEPTED_ORIGINS} = {})=> cors({
+    origin: (origin, callback)=>{
+        if (accepted_origins.includes(origin)) return callback(null,true)
+
+        if (!origin) return callback(null,true)
+
+        return callback(new Error('Unauthorized by cors policy'))
+
+    }
+})
