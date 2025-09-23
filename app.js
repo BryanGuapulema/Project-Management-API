f22ea2876dfa6bbe19a980a702fca3cbac3e2367
BryanGuapulema <bryan19012003@gmail.com>
2025-09-22 23:21:31 -0500

    middleware for errorHandle

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 import express from 'express'
-import { corsMiddleware } from './corsMiddleware.js'
+import { corsMiddleware } from './middlewares/corsMiddleware.js'
+import { errorHandler } from './middlewares/errorHandler.js'
 
 export const app = express()
 
 app.disable('x-powered-by')
 app.use(corsMiddleware())
+app.use(express.json())
+
+
+//ErrorHandler
+app.use(errorHandler)
