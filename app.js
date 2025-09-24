76b865dc14901779d5eca34681e5b99782cc543c
BryanGuapulema <bryan19012003@gmail.com>
2025-09-23 19:30:30 -0500

    check for user logged endpoint

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
 import express from 'express'
+import cookieParser from 'cookie-parser'
 import { corsMiddleware } from './middlewares/corsMiddleware.js'
 import { errorHandler } from './middlewares/errorHandler.js'
 import { userRouter } from './routes/UserRoutes.js'
 import { authRouter } from './routes/AuthRoutes.js'
 import { authMiddleware } from './middlewares/authMiddleware.js'
+import { AuthController } from './controllers/AuthController.js'
+import { catchAsync } from './utils/catchAsync.js'
 
 export const app = express()
 
 app.disable('x-powered-by')
 app.use(corsMiddleware())
+app.use(cookieParser());
 app.use(express.json())
-//app.use(authMiddleware)
+app.use('/',authRouter)
+
+app.use(authMiddleware)
+app.get('/auth/me',catchAsync(AuthController.me))
 
 
 //routes
 app.use('/users',userRouter)
-app.use('/',authRouter)
 
 //ErrorHandler
 app.use(errorHandler)
